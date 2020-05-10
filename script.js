825f089b1a7e07ba9d544da5bf0580b65352ad68
Cyhyraethz <cyhyraethz@gmail.com>
2020-05-10 14:39:39 -0700

    Fill in square with player's symbol when player clicks on it

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+let counter = 0;
+let roundNumber = 0;
+const round = document.getElementById('round');
+round.innerHTML = `<h3>Round ${roundNumber}</h3>`;
+
 const playerFactory = (name, symbol) => {
+  const topLeft = document.getElementById('0').innerHTML;
+  const topMiddle = document.getElementById('1').innerHTML;
+  const topRight = document.getElementById('2').innerHTML;
+  const centerLeft = document.getElementById('3').innerHTML;
+  const centerMiddle = document.getElementById('4').innerHTML;
+  const centerRight = document.getElementById('5').innerHTML;
+  const bottomLeft = document.getElementById('6').innerHTML;
+  const bottomMiddle = document.getElementById('7').innerHTML;
+  const bottomRight = document.getElementById('8').innerHTML;
   let score = 0;
   const getName = () => name;
   const getScore = () => score;
   const getSymbol = () => symbol;
-  return { getName, getScore, getSymbol };
+  const winRound = () => {
+    if (topLeft === symbol && topMiddle === symbol && topRight === symbol) {
+      alert('You win this round!');
+      round++;
+      score++;
+    }
+  };
+  return { getName, getScore, getSymbol, winRound };
 };
 
-const playerX = playerFactory(prompt('Name of Player X: '), 'X');
-const playerO = playerFactory(prompt('Name of Player O: '), 'O');
+// const playerX = playerFactory(prompt('Name of Player X: '), 'X');
+// const playerO = playerFactory(prompt('Name of Player O: '), 'O');
+const playerX = playerFactory('Dylan', 'X');
+const playerO = playerFactory('Arlo', 'O');
 
 const gameBoard = (() => {
-  const boardState = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+  const boardState = [];
   const populateBoard = () => {
     for (let i = 0; i < boardState.length; i++) {
       let square = document.getElementById(i);
@@ -21,8 +44,37 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-  const playerXInput = document.getElementById('x');
-  const playerOInput = document.getElementById('o');
-  playerXInput.innerHTML = `${playerX.getName()} ${playerX.getScore()}`;
-  playerOInput.innerHTML = `${playerO.getName()} ${playerO.getScore()}`;
+  const playerXScore = document.getElementById('x');
+  const playerOScore = document.getElementById('o');
+  playerXScore.innerHTML = `${playerX.getName()} ${playerX.getScore()}`;
+  playerOScore.innerHTML = `${playerO.getName()} ${playerO.getScore()}`;
 })();
+
+for (let i = 0; i < 9; i++) {
+  const square = document.getElementById(i);
+  square.addEventListener('click', function () {
+    if (square.innerHTML === '') {
+      if (round % 2 === 0) {
+        if (counter % 2 === 0) {
+          square.innerHTML = 'X';
+          playerX.winRound();
+          counter++;
+        } else if (counter % 2 !== 0) {
+          square.innerHTML = 'O';
+          playerO.winRound();
+          counter++;
+        }
+      } else if (round % 2 !== 0) {
+        if (counter % 2 !== 0) {
+          square.innerHTML = 'X';
+          playerX.winRound();
+          counter++;
+        } else if (counter % 2 === 0) {
+          square.innerHTML = 'O';
+          playerO.winRound();
+          counter++;
+        }
+      }
+    }
+  });
+}
