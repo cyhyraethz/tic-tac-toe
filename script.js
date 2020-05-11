83555f8b9c861d975282bf38590f618b17818225
Cyhyraethz <cyhyraethz@gmail.com>
2020-05-11 10:59:52 -0700

    Add logic for ending game

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 let round = 0;
 let counter = 0;
+let boardState = [];
+const topLeft = document.getElementById('0');
+const topMiddle = document.getElementById('1');
+const topRight = document.getElementById('2');
+const centerLeft = document.getElementById('3');
+const centerMiddle = document.getElementById('4');
+const centerRight = document.getElementById('5');
+const bottomLeft = document.getElementById('6');
+const bottomMiddle = document.getElementById('7');
+const bottomRight = document.getElementById('8');
 const roundDisplay = document.getElementById('round');
-roundDisplay.innerHTML = `<h3>Round ${round} &nbsp;&nbsp; Turn ${counter}</h3>`;
+
+const playSquare = (() => {
+  for (let i = 0; i < 9; i++) {
+    const square = document.getElementById(i);
+    square.addEventListener('click', function () {
+      if (square.innerHTML === '') {
+        if (round % 2 === 0) {
+          if (counter % 2 === 0) {
+            square.innerHTML = 'X';
+            counter++;
+            render();
+            playerX.winRound();
+          } else if (counter % 2 !== 0) {
+            square.innerHTML = 'O';
+            counter++;
+            render();
+            playerO.winRound();
+          }
+        } else if (round % 2 !== 0) {
+          if (counter % 2 !== 0) {
+            square.innerHTML = 'X';
+            counter++;
+            render();
+            playerX.winRound();
+          } else if (counter % 2 === 0) {
+            square.innerHTML = 'O';
+            counter++;
+            render();
+            playerO.winRound();
+          }
+        }
+      }
+    });
+  }
+})();
 
 const playerFactory = (name, symbol) => {
-  const topLeft = document.getElementById('0').innerHTML;
-  const topMiddle = document.getElementById('1').innerHTML;
-  const topRight = document.getElementById('2').innerHTML;
-  const centerLeft = document.getElementById('3').innerHTML;
-  const centerMiddle = document.getElementById('4').innerHTML;
-  const centerRight = document.getElementById('5').innerHTML;
-  const bottomLeft = document.getElementById('6').innerHTML;
-  const bottomMiddle = document.getElementById('7').innerHTML;
-  const bottomRight = document.getElementById('8').innerHTML;
   let score = 0;
   const getName = () => name;
   const getScore = () => score;
   const getSymbol = () => symbol;
   const winRound = () => {
-    if (topLeft === symbol && topMiddle === symbol && topRight === symbol) {
+    if (
+      topLeft.innerHTML === symbol &&
+      topMiddle.innerHTML === symbol &&
+      topRight.innerHTML === symbol
+    ) {
       alert('You win this round!');
       round++;
       score++;
+      counter = 0;
+      clearBoard();
+      render();
     }
   };
   return { getName, getScore, getSymbol, winRound };
@@ -32,8 +74,8 @@ const playerFactory = (name, symbol) => {
 const playerX = playerFactory('Dylan', 'X');
 const playerO = playerFactory('Arlo', 'O');
 
-const gameBoard = (() => {
-  const boardState = [];
+const clearBoard = () => {
+  boardState = ['', '', '', '', '', '', '', '', ''];
   const populateBoard = () => {
     for (let i = 0; i < boardState.length; i++) {
       let square = document.getElementById(i);
@@ -41,40 +83,14 @@ const gameBoard = (() => {
     }
   };
   populateBoard();
-})();
+};
 
-const displayController = (() => {
+const render = () => {
   const playerXScore = document.getElementById('x');
   const playerOScore = document.getElementById('o');
   playerXScore.innerHTML = `${playerX.getName()} ${playerX.getScore()}`;
   playerOScore.innerHTML = `${playerO.getName()} ${playerO.getScore()}`;
-})();
+  roundDisplay.innerHTML = `<h3>Round ${round} &nbsp;&nbsp; Turn ${counter}</h3>`;
+};
 
-for (let i = 0; i < 9; i++) {
-  const square = document.getElementById(i);
-  square.addEventListener('click', function () {
-    if (square.innerHTML === '') {
-      if (round % 2 === 0) {
-        if (counter % 2 === 0) {
-          square.innerHTML = 'X';
-          playerX.winRound();
-          counter++;
-        } else if (counter % 2 !== 0) {
-          square.innerHTML = 'O';
-          playerO.winRound();
-          counter++;
-        }
-      } else if (round % 2 !== 0) {
-        if (counter % 2 !== 0) {
-          square.innerHTML = 'X';
-          playerX.winRound();
-          counter++;
-        } else if (counter % 2 === 0) {
-          square.innerHTML = 'O';
-          playerO.winRound();
-          counter++;
-        }
-      }
-    }
-  });
-}
+render();
