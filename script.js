b8076f75ce8d0c80ed61cbc4132ba3c20d4056fb
Cyhyraeth <cyhyraethz@gmail.com>
2020-10-22 14:10:45 -0700

    Add render method to gameBoard module

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// module containing the state of the game
 const gameBoard = (() => {
-  // 
+  let gameBoard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+  const render = () => {
+    gameBoard.forEach((value, index) => {
+      const square = document.getElementById(index.toString());
+      square.innerHTML = value;
+    })
+  }
+  render();
 })();
+
+// module for controlling the display
 const displayController = (() => {
   // 
 })();
+
+// factory for creating new players
 const playerFactory = (name) => {
   const getName = () => name;
 };
