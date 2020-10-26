eff9be21b38be577772fcdcc3359a0de9a23786e
Cyhyraeth <cyhyraethz@gmail.com>
2020-10-25 19:18:24 -0700

    Add horizontal win condition

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 // module containing the state of the game
 const board = (() => {
+  let _player;
   let _count = 0;
   let _state = ['', '', '', '', '', '', '', '', ''];
   const _gameOver = () => {
+    let msg = '';
+    for (let i = 0; i < 6; i += 3) {
+      if (_state[i] !== '' && _state[i] === _state[i + 1] && _state[i] === _state[i + 2]) {
+        msg = _player.getName() + " wins!";
+      }
+    }
     if (_count === _state.length) {
-      setTimeout(function(){alert('Game over!')}, 0);
+      msg = "It's a tie!";
+    }
+    if (msg !== '') {
+      setTimeout(function(){alert(msg)}, 0);
     }
   }
   const getState = () => _state;
   const setState = (index) => {
     if (_state[parseInt(index)] === '') {
       if (_count % 2 === 0) {
-        _state[parseInt(index)] = player1.getSymbol();
+        _player = player1;
       } else {
-        _state[parseInt(index)] = player2.getSymbol();
+        _player = player2;
       }
+      _state[parseInt(index)] = _player.getSymbol();
       _count++;
     }
     display.render();
