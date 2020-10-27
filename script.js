e30686392af191fbf2db3dedcb727b33975d8c08
Cyhyraeth <cyhyraethz@gmail.com>
2020-10-27 12:07:50 -0700

    Switch which player goes first after each round

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // module containing the state of the game
 const board = (() => {
   let _player; // player who's turn it is
+  let _round = 0; // number of rounds that have been played
   let _count = 0; // number of moves that have been played, used to determine whose turn it is
   let _state = ['', '', '', '', '', '', '', '', '']; // state of the board, initialized as an empty board
   let _score = [0, 0]; // number of rounds won by each player
@@ -15,8 +16,7 @@ const board = (() => {
   // private method that checks if the game is over and announces the winner, or a tie, if it is
   const _gameOver = () => {
     let msg; // message to announce if game is over
-    // list of win conditions, indices of consecutive squares
-    const win = [
+    const win = [ // list of win conditions, indices of consecutive squares
       [0, 1, 2], // top row
       [3, 4, 5], // middle row
       [6, 7, 8], // bottom row
@@ -26,21 +26,19 @@ const board = (() => {
       [0, 4, 8], // diagonal down
       [2, 4, 6], // diagonal up
     ];
-    // if any of the win conditions has been met, assign a string announcing the winner
-    win.forEach(a => {
+    win.forEach(a => { // if any of the win conditions has been met, assign a string announcing the winner
       if (_state[a[0]] && _state[a[0]] === _state[a[1]] && _state[a[0]] === _state[a[2]]) {
         msg = _player.getName() + " wins!";
       }
     });
-    // if every square is filled in, assign a string to msg announcing a tie
-    if (_count === _state.length) {
+    if (_count === _state.length) { // if every square is filled in, assign a string to msg announcing a tie
       msg = "It's a tie!";
     }
-    // if the current round is over, announce the winner and reset the board
-    if (typeof msg === 'string') {
+    if (typeof msg === 'string') { // if the current round is over, announce the winner and reset the board
       setTimeout(function(){
         alert(msg); // anounce the winner
         _reset(); // reset the board
+        _round++;
       }, 0);
     }
   }
@@ -51,10 +49,18 @@ const board = (() => {
   // public method that fills in an empty square with the current player's symbol
   const setState = (index) => {
     if (_state[parseInt(index)] === '') { // checks if the selected square is empty
-      if (_count % 2 === 0) {
-        _player = player1; // if an even number of moves have been played, it's player1's turn
-      } else {
-        _player = player2; // if an odd number of moves have been played, it's player2's turn
+      if (_round % 2 === 0) { // if an even number of rounds have been played, player1 goes first
+        if (_count % 2 === 0) {
+          _player = player1; // if an even number of moves have been played, it's player1's turn
+        } else {
+          _player = player2; // if an odd number of moves have been played, it's player2's turn
+        }
+      } else { // if an odd number of rounds have been played, player2 goes first
+        if (_count % 2 === 0) {
+          _player = player2; // if an even number of moves have been played, it's player2's turn
+        } else {
+          _player = player1; // if an odd number of moves have been played, it's player1's turn
+        }
       }
       _state[parseInt(index)] = _player.getSymbol(); // fills in the selected square with the current player's symbol
       _count++; // increments the _count variable
